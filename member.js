1471ba58c159a3783688abbb8e0dcdf42b59923a
Donald Uviase <37631734+tekwok@users.noreply.github.com>
2023-08-12 18:14:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsmember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var member1 = document.getElementById("member1");
+    var skills1 = document.getElementById("skills1");
+    var contact1 = document.getElementById("contact1");
+    var about1 = document.getElementById("about1");
+    member1.style.display = "block";
+    skills1.style.display = "none";
+    contact1.style.display = "none";
+    about1.style.display = "none";
+    member.style.color = "rgb(0, 0, 0)";
+    skills.style.color = "rgb(255, 255, 255)";
+    contact.style.color = "rgb(255, 255, 255)";
+    about.style.color = "rgb(255, 255, 255)";
+}
